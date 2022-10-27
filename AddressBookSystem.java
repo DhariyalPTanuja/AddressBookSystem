6d15690942f27479ec2bb3e6a7b96adbb04c4c7e
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-10-27 15:03:13 +0530

    welcome message

---
+++ AddressBookSystem.java
@@ -0,0 +1,10 @@
+package com.adddresscontactbooksystem;
+
+public class AddressBookSystem {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome to Address Book Program");
+
+    }
+}
